ccd66dba54ba8f5582844af8c9a9a882b3a9bd8e
Noe Javet <javetn@student.ethz.ch>
2018-05-24 20:13:12 +0200

    added config files

--- logbot.py
+++ logbot.py
@@ -13,6 +13,21 @@ class LogBot(object):
     def get_keys(self):
         return self._keys
 
+    def get_field(self, key):
+        return self.get_keys()[key]
+
+    def get_category(self, key):
+        if
+    def get_dict(self, key):
+        pass
+
+    def analyze_key(self, key):
+        if len(key) != 2:
+            return None
+        elif key[0] == 'S':
+
+
+
     # update database
     def update(self, collection, dix):
         dix['datetime'] = datetime.datetime.today()
@@ -22,4 +37,7 @@ class LogBot(object):
     # message handler
     def analyze_msg(self, bot, update):
         msg = update.message.text
+        words = msg.split(' ')
+        key = words[0]
+        payload = words[1:]
 
